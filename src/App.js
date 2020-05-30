488458637893c4d77958073207c617949e3aad32
jacky <jackyo.dev@gmail.com>
2020-05-29 22:14:19 -0400

    weather api call

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
-import React, {Component} from 'react';
-import { Route, NavLink } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Switch, Route, NavLink } from 'react-router-dom'
+import axios from 'axios'
 
+import { WeatherAPIKey } from "./key"
 import './App.css';
-import { Key } from "./key"
+
+
+// const log = (message) => { console.log(message, "LOG HERE") }
+
+
+import { currentWeatherContainer } from './currentWeather'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGeoOn: false,
+      current_coords: "",
+      search_location: "",
+      location_weather: "",
+    }
+  }
+
+  setCoords(position) {
+    let x = position.coords.latitude;
+    let y = position.coords.longitude;
+    let coords = { latitude: x, longitude: y }
+
+    this.callWeatherApi(coords)
+  }
 
 
+  getGeoLocation(event) {
+    // event.preventDefault()
+    if (navigator.geolocation) {
+      this.setState({
+        isGeoOn: true
+      })
+      navigator.geolocation.getCurrentPosition((position) => { this.setCoords(position) })
+    }
+    else {
+      this.setState({
+        isGeoOn: false
+      })
+
+    }
+  }
 
 
-  render () {
+  callWeatherApi(props) {
+    let { longitude, latitude } = props;
+    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WeatherAPIKey}&units=imperial`;
+
+    axios.get(api).then((res) => this.setState({
+      current_coords: res.data.coord,
+      location_weather: {
+        temp: res.data.main.temp,
+        min_temp: res.data.main.temp_min,
+        max_temp: res.data.main.temp_max,
+        weather_title: res.data.weather[0].main,
+        weather_desc: res.data.weather[0].description,
+        icon: res.data.weather[0].icon
+      }
+
+    })).then(() => { console.log(this.state) })
+
+      ;
+  }
+
+
+  componentDidMount() {
+    this.getGeoLocation();
+  }
+
+  render() {
+    let weather = this.state.location_weather
+    console.log(this.state)
     return (
       <>
-    <div className = "navigation__container">
-      
-    </div> 
-    </>
+        <div className="navigation__container">
+          <NavLink to="/current"> Current </NavLink>
+          <NavLink to="/week">7-Days Forecast</NavLink>
+          <input type="text" placeholder="Search" />
+        </div>
+
+        <Switch>
+          <Route exact path="/">{currentWeatherContainer(weather)}</Route>
+        </Switch>
+      </>
     )
   }
 }
---
+++ src/currentWeather.js
@@ -0,0 +1,36 @@
+
+
+
+import React from "react";
+import image from "./icons/animated/cloudy-day-1.svg"
+
+
+
+export const currentWeatherContainer = (weather) =>{
+
+ if(weather){
+
+  return (
+    <div className="current__container">
+     <div className="current__weather">
+      <div className ="weather__icon__container">
+      <img className = "weather__icon" alt = "" src = {image} />
+      </div>
+      <div className="weather__header">
+       <h2 className="header">{Math.round(weather.temp)}°</h2>
+      </div>
+      <div className = "weather__info"> 
+       <h3> {weather.weather_title}</h3>
+       <h3> {weather.weather_desc}</h3>
+       <p> Max Temp: {Math.round(weather.max_temp)}°</p>
+       <p> Min Temp: {Math.round(weather.min_temp)}° </p></div>
+     </div>
+    </div>
+  )
+ }
+ else {
+  return (<h1> Loading ...</h1>)
+ }
+
+
+}
